23b844463b346c3c3c321629b13f44d8f50d4fa1
vhivestate <garcia.valeria001@gmail.com>
2022-10-12 04:44:06 -0600

    landing page

--- src/pages/Landing/index.js
+++ src/pages/Landing/index.js
@@ -2,6 +2,7 @@ import React from 'react'
 import '../../App.css'
 import Headshot from '../../assests/images/headshot.png'
 import { Link } from 'react-router-dom';
+import { ArrowDownCircle } from 'react-feather';
 
 function Landing(){
     return(
@@ -12,28 +13,118 @@ function Landing(){
                     <div className="max-w-md">
                         <h1 className="mb-5 text-5xl font-bold">WEB DEVELOPER</h1>
                         <p className="mb-5">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
-                        <button className="btn btn-active text-white">Get Started</button>
+                        <button className=' animate-bounce'><a href='#about'><ArrowDownCircle></ArrowDownCircle></a></button>
                     </div>
                 </div>
             </div>
 
+    {/* ABOUT */}
+
+            <h2 id='start' className='landing-skills text-center pt-10 text-5xl	bg-white'>ABOUT</h2>
             <div className='bg-white'>
-              <h2>ABOUT</h2>
-                <div className='about pt-14'>
-                    <div className="card lg:card-side w-10/12 p-5 glass">
-                            <figure><img src={Headshot} alt="Album"/></figure>
-                        <div className="card-body justify-around ">
-                            <h2 className="card-title">Valeria Garcia</h2>
-
-                        <div className='card-text'>
-                            <p className='min-w-fit'>Full-stack web developer based out of Salt Lake City, Utah. Graduated from The University of Utah. Here to provide everything you will need for your ideal website. </p>
-                        </div>
-
-                        <div className='flex flex-wrap p-3 justify-center'>
-                            <button className="about-btn btn btn-active btn-wide text-white"><Link to={'/projects'}>View Work</Link></button>  
-                        </div>
-                        </div>
+            <div className='about pt-14'>
+            <div className="card lg:card-side w-10/12 p-5 glass">
+            <figure><img src={Headshot} alt="Album"/></figure>
+            <div className="card-body justify-around ">
+                <h2 className="card-title">Valeria Garcia</h2>
+                <div className='card-text'>
+                <p className='min-w-fit'>Full-stack web developer based out of Salt Lake City, Utah. Graduated from The University of Utah. Here to provide everything you will need for your ideal website. </p>
+                </div>
+                <div className='flex flex-wrap p-3 justify-center'>
+                <button className="about-btn btn btn-active  text-white"><Link to={'/projects'}>View Portfolio</Link></button>  
                     </div>
+                </div>
+            </div>
+            </div>
+
+
+               {/* services */}
+
+            <div className='services flex flex-wrap justify-center pt-9'>
+                <div className="card w-96 bg-neutral ">
+                    <div className="card-body items-center text-center">
+                    <h2 className="card-title underline">WEB DEVELOPMENT</h2>
+                    <div className='frontend'>
+                        <dl>
+                            <dt>Landing Page</dt>
+                            <dt>Ecommerce Website</dt>
+                            <dt>Booking Website</dt>
+                            <dt>Management System</dt>
+                            <dt>Contact Form</dt>
+                            <dt>Portfolio</dt>
+                        </dl>
+                        </div>
+                    <div className="card-actions justify-end">
+
+                    </div>
+                </div>
+                </div>
+            </div>
+
+  <div className="divider pt-9"></div> 
+
+{/* skills */}
+<h2 className='landing-skills text-center pt-10 text-5xl	'>SKILLS</h2>
+  <div className='resume-skills flex flex-wrap justify-center p-8 space-x-4 '>
+    <div className="card w-96 bg-neutral ">
+      <div className="card-body items-center text-center">
+        <h2 className="card-title">FRONTEND</h2>
+        <div className='frontend'>
+            <dl>
+                <dt>HTML:</dt>
+                    <dd> - Forms & Validation</dd>
+                    <dd> - Semantic Tags</dd>
+                    <dd> - Accessibility</dd>
+                <dt>CSS:</dt>
+                    <dd>- Flexbox</dd>
+                    <dd>- Responsive Design</dd>
+                    <dd>- Media Queries</dd>
+                <dt>JavaScript:</dt>
+                    <dd> - ES6 + Syntax </dd>
+                    <dd> - DOM Manipulation </dd>
+                    <dd> - Fetch API or AJAX</dd>
+                    <dd> - Hoisting, Closures, Event Bubbling</dd>
+            </dl>
+            </div>
+        <div className="card-actions justify-end">
+
+        </div>
+      </div>
+    </div>
+
+    <div className="nav-a divider divider-horizontal text-xl">&</div>
+
+    <div className="card w-96 bg-neutral">
+      <div className="card-body items-center text-center">
+        <h2 className="card-title">BACKEND</h2>
+        <div className='backend'>
+          
+            <dl>
+                <dt>Node.js</dt>
+                <dt>Database:</dt>
+                    <dd>- MongoDB</dd>
+                    <dd>- GraphQL</dd>
+                    <dd>- MySQL</dd>
+                <dt>API:</dt>
+                    <dd>- REST</dd>
+                    <dd>- JSON</dd>
+                    <dd>- SOAP</dd>
+                <dt>Express.js</dt>
+                <dt>Frameworks:</dt>
+                    <dd>- React.js</dd>
+                    <dd>- Bootstrap</dd>
+                    <dd>- Tailwind</dd>
+            </dl>
+            </div>
+        <div className="card-actions justify-end">
+        </div>
+      </div>
+    </div>
+
+    </div>
+
+                <div>
+                    <h2>Contact</h2>
                 </div>
           </div>
         </div>
